ca7f791e04696de3f687039e95e5a3f1db686681
Omer Odabasoglu <omerpasha34@gmail.com>
2017-03-17 09:48:30 -0700

    Practice controller setup, environments, packages

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -3,8 +3,58 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Rych\Random\Random;
 
 class PracticeController extends Controller
 {
-    //
+    /**
+    * ANY (GET/POST/PUT/DELETE)
+    * /practice/{n?}
+    *
+    * This method accepts all requests to /practice/ and
+    * invokes the appropriate method.
+    */
+    public function index($n) {
+
+          $method = 'practice'.$n;
+          if(method_exists($this, $method))
+              return $this->$method();
+          else
+              dd("Pratice route [{$n}] not defined");
+
+    }
+
+    /*
+    * example
+    */
+    public function practice1() {
+        dump('This is the first example.');
+    }
+
+    /*
+    * example 2 using config function
+    */
+    public function practice2() {
+        dump(config('app'));
+    }
+
+    /*
+    * Rych example
+    */
+    public function practice3() {
+        $random = new Random();
+
+        // Generate a 16-byte string of random raw data
+        $randomBytes = $random->getRandomBytes(16);
+        dump($randomBytes);
+
+        // Get a random integer between 1 and 100
+        $randomNumber = $random->getRandomInteger();
+        dump($randomNumber);
+        // Get a random 8-character string using the
+        // character set A-Za-z0-9./
+        $randomString = $random->getRandomString(8);
+        dump($randomString);
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ Route::get('/books', 'BookController@index');
 Route::get('/books/{title?}', 'BookController@view');
 
 Route::get('/', 'WelcomeController');
+
+/**
+* Practice
+*/
+Route::any('/practice/{n?}', 'PracticeController@index');
+
+if(config('app.env') == 'local') {
+    Route::get('/logs', function(){ });
+}
+
+if(config('app.env') == 'local') {
+    Route::get('/logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
+}
